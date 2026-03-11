2e2500bcf644a8674ad7ff9508afba7c798a8cb0
Azhari <rebeliviouscreative@gmail.com>
2026-03-11 09:52:38 +0800

    Move assets to public and hardcode image path

--- app/[...urlSegments]/page.tsx
+++ app/[...urlSegments]/page.tsx
@@ -5,11 +5,11 @@ import client from '../../tina/__generated__/client';
 export default function HomePage() {
   return (
     <main style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: '#000' }}>
-      <img 
-        src="/assets/main-logo.png" 
-        alt="Crossfield Logo" 
-        style={{ width: '250px', marginBottom: '2rem' }} 
-      />
+     <img 
+      src="/CrossFieldCollective/assets/main-logo.png" 
+       alt="Crossfield Logo" 
+      style={{ width: '250px', marginBottom: '2rem' }} 
+    />
       
       <h1 style={{ fontSize: '3rem', fontWeight: 'bold', letterSpacing: '-0.05em', color: '#fff' }}>
         CROSSFIELD <span style={{ color: '#990000' }}>WEB</span>
