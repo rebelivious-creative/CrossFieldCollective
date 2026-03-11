aefe5aa44f1ab9fd3d6e37bbdf93f78582f87447
Azhari <rebeliviouscreative@gmail.com>
2026-03-11 09:45:53 +0800

    Update next.config.ts for GitHub Pages routing

--- next.config.ts
+++ next.config.ts
@@ -1,55 +1,11 @@
-import type { NextConfig } from 'next'
- 
+import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
-  // This tells Next.js to ignore TypeScript errors and force the build
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  // We are also muting strict linting errors just to be safe
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  output: "export",
+  basePath: "/CrossFieldCollective",
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'assets.tina.io',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-      }
-    ],
-  },
-  async headers() {
-    // these are also defined in the root layout since github pages doesn't support headers
-    const headers = [
-      {
-        key: 'X-Frame-Options',
-        value: 'SAMEORIGIN',
-      },
-      {
-        key: 'Content-Security-Policy',
-        value: "frame-ancestors 'self'",
-      },
-    ];
-    return [
-      {
-        source: '/(.*)',
-        headers,
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/admin',
-        destination: '/admin/index.html',
-      },
-    ];
+    unoptimized: true,
   },
 };
 
-export default nextConfig
+export default nextConfig;
