1e4220f569865ce3d4898123e23785a5be368543
Azhari <rebeliviouscreative@gmail.com>
2026-03-11 11:16:34 +0800

    Remove GitHub pathing to restore Vercel styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import client from '../tina/__generated__/client';
-import ClientPage from './client-page';
-
-export default async function HomePage() {
-  const res = await client.queries.page({ relativePath: 'home.md' });
-  return <ClientPage {...res} />;
-}
+<img 
+  src="/assets/main-logo.png" 
+  alt="Crossfield Logo" 
+  style={{ width: '250px', marginBottom: '2rem' }} 
+/>
