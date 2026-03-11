677cfaaed0f4be5b088f03b290b6d941cfc428e3
Azhari <rebeliviouscreative@gmail.com>
2026-03-12 02:49:47 +0800

    update next config for cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // Tells Next.js to build a pure static site for Cloudflare
   images: {
     unoptimized: true,
   },
