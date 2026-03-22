64a445d8622273135ec884568a847732b81d606f
Azhari <rebeliviouscreative@gmail.com>
2026-03-23 04:43:56 +0800

    Added missing generateStaticParams for cloud build

--- app/[...urlSegments]/page.tsx
+++ app/[...urlSegments]/page.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import client from "../../tina/__generated__/client";
 import ClientPage from "../client-page";
 
-export default async function Page({ params }) {
+export default async function Page({ params }: any) {
   const { urlSegments } = await params;
 
-  // Fetch the data for the specific page (e.g., services.md)
+  // Fetch the data for the specific page (e.g., vision.md)
   const res = await client.queries.pages({
     relativePath: `${urlSegments.join('/')}.md`,
   });
 
-  // Send it to the template's brain
   return <ClientPage {...res} />;
 }
 
